345643606e8112d1d16b1fca01f3ab2d5c8554ee
drmr <126770071+tv-drmr@users.noreply.github.com>
2023-04-18 16:25:29 +0200

    d

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ packages = [
 
 setup(
     name="liftcord.py-tools",
-    version="12.0.3",
+    version="12.0.4",
     packages=packages,
     include_package_data=True,
     license="MIT License",
